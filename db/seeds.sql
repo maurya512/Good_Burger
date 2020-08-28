757880a3b09c192dc1abf923797bfbe7b51bab9b
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-28 10:11:44 -0400

    the data that's going to go inside the burger database

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+INSERT INTO burgers (burger_name, devour) VALUES ('impossible burger', TRUE);
+INSERT INTO burgers (burger_name, devour) VALUES ('turkey bacon deluxe', TRUE);
+INSERT INTO burgers (burger_name) VALUES ('chicken burger');
+INSERT INTO burgers (burger_name, devour) VALUES ('salmon sliders');
