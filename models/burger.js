2b2841fa71c8a24fca4da398cdee575006ec3262
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-29 17:01:40 -0400

    the name of the variables was changed

--- models/burger.js
+++ models/burger.js
@@ -2,6 +2,7 @@
 var orm = require("../config/orm.js");
 
 var burger = {
+  // read/ select all the record in the database
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
@@ -9,16 +10,19 @@ var burger = {
   },
 
   create: function(cols, vals, cb) {
+    // create/insert a new record to the database
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
+    // update a specfic record in the database
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
+    // delete a specific record in the database
     orm.delete("burgers", condition, function(res) {
       cb(res);
     });
