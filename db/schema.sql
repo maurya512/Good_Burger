fefe7bbcf5ab0833cc9a1c7509e0245b937312e2
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-31 08:09:51 -0400

    changed the schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,9 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT, 
     burger_name VARCHAR(255) NOT NULL,
-    devour BOOLEAN DEFAULT false
+    devour BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
 );
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO burgers (burger_name) VALUES ('hamburger');
 INSERT INTO burgers (burger_name, devour) VALUES ('impossible burger', TRUE);
 INSERT INTO burgers (burger_name, devour) VALUES ('turkey bacon deluxe', TRUE);
 INSERT INTO burgers (burger_name) VALUES ('chicken burger');
-INSERT INTO burgers (burger_name, devour) VALUES ('salmon sliders');
+INSERT INTO burgers (burger_name, devour) VALUES ('salmon sliders', TRUE);
