9d749708bde67d5f632fd1c6dcf1fd8149f14854
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-31 08:09:04 -0400

    tweaked orm file

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,11 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-// helper function to print the question marks
+// Helper function for SQL syntax.
+// Let's say we want to pass 3 values into the mySQL query.
+// In order to write the query, we need 3 question marks.
+// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
 
@@ -99,4 +103,5 @@ var orm = {
   }
 };
 
+// Export the orm object for the model (cat.js).
 module.exports = orm;
