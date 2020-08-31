d6f5250e2774c7b59ec5d85d4034805594ba590d
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-31 08:10:47 -0400

    adjusted the code to reflect changes made in other files

--- models/burger.js
+++ models/burger.js
@@ -2,27 +2,23 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-  // read/ select all the record in the database
   all: function(cb) {
     orm.all("burgers", function(res) {
       cb(res);
     });
   },
-
+  // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    // create/insert a new record to the database
     orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    // update a specfic record in the database
     orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    // delete a specific record in the database
     orm.delete("burgers", condition, function(res) {
       cb(res);
     });
